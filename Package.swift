ba51efa5e25765c363198b905ad8ba216793aa26
Alessio la Penna <alessio.lapenna@gmail.com>
2022-10-06 11:30:35 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.4
 
 import PackageDescription
 
 let package = Package(
     name: "Didomi",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
     products: [
         .library(
             name: "Didomi",
             targets: ["Didomi"]
         ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
         .binaryTarget(
