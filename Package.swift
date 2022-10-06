28fd96056c013f255a873523f93226b9b163e484
Alessio la Penna <alessio.lapenna@gmail.com>
2022-10-06 11:35:19 +0900

    xcframework import

--- Package.swift
+++ Package.swift
@@ -19,8 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Didomi",
-            url: "https://sdk.didomi.io/ios/didomi-ios-sdk-1.76.0-xcframework.zip",
-            checksum: "4b807408e87d8d93ea4be9f2425ed9c59f252de837b0a487d334ec1ab94840fb"
+            path: "Didomi.xcframework"
         )
     ]
 )
